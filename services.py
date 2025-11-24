0d61fb775f6b08372c5f58221f61245ba44dd77e
prakhar-paliwal-07 <prakharpaliwal858@gmail.com>
2025-11-24 15:33:42 +0530

    edit services
    Added comments to clarify the purpose of each function.

--- services.py
+++ services.py
@@ -1,17 +1,18 @@
-def add_expense(expenses, desc, amt, cat, date):
-    new_id=len(expenses) + 1
-    exp = {"id": new_id,"description": desc,"amount": amt,"category": cat,"date": date}
+def add_expense(expenses, desc, amt, cat, date): # defining add expense function
+    new_id=len(expenses)+1
+    exp={"id": new_id,"description": desc,"amount": amt,"category": cat,"date": date}
     expenses.append(exp)
     return exp
-def add_income(incomes, src, amt, date):
+def add_income(incomes, src, amt, date):  # defining the add income function
     new_id=len(incomes) + 1
     inc={"id": new_id,"source": src,"amount": amt,"date": date}
     incomes.append(inc)
     return inc
 
-def total_expenses(expenses):
+def total_expenses(expenses): # definig the total expense expense function
     return sum(e["amount"] for e in expenses)
-def total_incomes(incomes):
+def total_incomes(incomes): # defining total income function
     return sum(i["amount"] for i in incomes)
-def net_balance(expenses, incomes):
+def net_balance(expenses,incomes):  # defining the net balance function
     return total_incomes(incomes) - total_expenses(expenses)
+
