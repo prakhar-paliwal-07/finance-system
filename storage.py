827530998214488b9d068d7eb47efc05a10c5a8e
prakhar-paliwal-07 <prakharpaliwal858@gmail.com>
2025-11-24 15:35:01 +0530

    edit storage

--- storage.py
+++ storage.py
@@ -1,18 +1,20 @@
-import json
-import os
+import json  # importing json module for storage
 
-FILE_PATH = "finance_data.json"
+import os  #   importing  os
+
+FILE_PATH="finance_data.json"  # this is the file which has all the data entered and stored by a programmerr
 
 
-def load_data():
+def load_data(): # defining load data function
     if not os.path.exists(FILE_PATH):
-        return [], []
+      return [],[]
 
     with open(FILE_PATH, "r") as file:
-        data = json.load(file)
-        return data.get("expenses", []), data.get("incomes", [])
+        data=json.load(file)
+    return data.get("expenses",[]),data.get("incomes", [])
 
 
-def save_data(expenses, incomes):
-    with open(FILE_PATH, "w") as file:
+def save_data(expenses, incomes): # defining save_data function to 
+    with open(FILE_PATH,"w") as file  :
         json.dump({"expenses": expenses, "incomes": incomes}, file)
+        
